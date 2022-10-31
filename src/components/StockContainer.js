d01cdad6033957b1553a0374e1b1449a0beacaa9
Chan Lee <clee09818@gmail.com>
2022-10-31 01:01:41 -0400

    commit tree 6394daa566dbedca1127bc54c74429697536d86d

--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Stock from "./Stock";
 
 function StockContainer() {
+
+const [stockItem, setStockItem] = useState([])
+
+useEffect(() => {
+  fetch(`http://localhost:3001/stocks`)
+    .then((r) => r.json())
+    .then((data) => {
+      console.log(data);
+      setStockItem(data);
+    })
+}, [])
+
   return (
     <div>
       <h2>Stocks</h2>
-      {/* render stock list here*/}
+      {stockItem.map((item) => (
+        <Stock key={item.id} item={item} />
+      ))}
     </div>
   );
 }
